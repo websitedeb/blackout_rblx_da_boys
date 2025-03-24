ef6b1b6a34b371aeb50e644838f2eab469959fb5
websitedeb <ghoshsar000@students.centergrove.k12.in.us>
2025-03-23 21:34:04 -0400

    added more systems, finished some

--- src/server/sound_Lobby.server.ts
+++ src/server/sound_Lobby.server.ts
@@ -1,11 +1,11 @@
 import { Workspace, ServerStorage } from "@rbxts/services";
 
-const soundPart : Instance | undefined = Workspace.FindFirstChild("Model")?.FindFirstChild("Sound");
+const soundPart : Instance | undefined = Workspace.FindFirstChild("Lobby")?.FindFirstChild("Sound");
 const sound : Instance | undefined = ServerStorage.FindFirstChild("Amber Glow");
 
 if ((soundPart && soundPart.IsA("Part")) && (sound && sound.IsA("Sound"))){
     sound.Parent = soundPart;
     sound.Looped = true;
-    sound.Volume = 2;
+    sound.Volume = 0.5;
     sound.Play();
 }
